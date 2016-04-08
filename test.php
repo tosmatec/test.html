47ab7e735f42b954140970bac0204dd7e845676c
tosmate c <tosmatec@gmail.com>
2016-04-08 15:31:15 +0700

    php
    testing

---
+++ test.php
@@ -0,0 +1,44 @@
+<?php
+function __autoload($class_name) {
+  require_once $class_name . '.php';
+}
+//include("/var/www/html/get/MyClass.php");
+
+echo "test <br/> ";
+
+
+
+//require_once('MyClass.php');
+
+echo "test .<br/>";
+
+ $vars = new MyClass;
+
+echo $vars->getProperty();
+echo $vars->setProperty("I'm a new property value!");
+echo $vars->getProperty();
+//print($vars->setProperty("I'm a new property value!"));
+
+echo "last test <br/>";
+
+/* require_once("/var/www/html/get/MyClass.php") or die ("Could Not load file");
+ //include("MyClass.php");
+
+ require_once('MyClass.php');
+*/
+
+$testObject = new MyClass;
+$testOthObj = new MyOtherClass;
+
+echo $testObject->getProperty();
+echo $testOthObj->newMethod();
+
+
+do
+{
+  // Call plusOne without instantiating MyClass
+  echo MyClass::plusOne();
+} while ( MyClass::$count < 10 );
+
+
+?>
